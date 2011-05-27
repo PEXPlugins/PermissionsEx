0da409254d7be7c41db92ea6fa4124126e02dcf4
t3hk0d3 <clouster@yandex.ru>
2011-05-27 23:01:20 +0400

    New Regexp library

--- src/main/java/ru/tehkode/permissions/PermissionEntity.java
+++ src/main/java/ru/tehkode/permissions/PermissionEntity.java
@@ -18,6 +18,8 @@
  */
 package ru.tehkode.permissions;
 
+import dk.brics.automaton.Automaton;
+import dk.brics.automaton.RegExp;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
@@ -28,118 +30,116 @@ import java.util.regex.Pattern;
  * @author code
  */
 public abstract class PermissionEntity {
-    
+
     protected PermissionManager manager;
     private String name;
     protected boolean virtual = true;
     protected String prefix = "";
     protected String suffix = "";
-    
+
     public PermissionEntity(String name, PermissionManager manager) {
         this.manager = manager;
         this.name = name;
-        
-        Logger.getLogger("Minecraft").info("New instance - \"" + name + "\"");
     }
-    
+
     public String getName() {
         return this.name;
     }
-    
+
     public boolean has(String permission, String world) {
         if (permission != null && permission.isEmpty()) { // empty permission for public access :)
             return true;
         }
-        
+
         String expression = getMatchingExpression(permission, world);
         if (expression != null) {
             return this.explainExpression(expression);
         }
-        
+
         return false;
     }
-    
+
     protected String getMatchingExpression(String permission, String world) {
         for (String expression : this.getPermissions(world)) {
-            if(getPattern(expression).matcher(permission).matches()){
+            if (isMatches(expression, permission)) {
                 return expression;
             }
         }
-        
+
         return null;
     }
-    
+
     public void setPermission(String permission, String value) {
         this.setOption(permission, value, "");
     }
-    
+
     public void removePermission(String permission) {
         this.removePermission(permission, "");
     }
-    
+
     public String getOptionValue(String permission, String world) {
         return this.getOption(permission, world, true);
     }
-    
+
     public String getOptionValue(String permission) {
         return this.getOption(permission, "", true);
     }
-    
+
     protected boolean explainExpression(String expression) {
         return !expression.substring(0, 1).equals("-"); // If expression have - (minus) before then that mean expression are negative
     }
-    
+
     public void setPermissions(String[] permission) {
         this.setPermissions(permission, null);
     }
-    
+
     public String getPrefix() {
         return this.prefix;
     }
-    
+
     public String getSuffix() {
         return this.suffix;
     }
-    
+
     public void setPrefix(String prefix) {
         this.prefix = prefix;
     }
-    
+
     public void setSuffix(String postfix) {
         this.suffix = postfix;
     }
-    
+
     @Override
     public String toString() {
         return this.getName();
     }
-    
+
     public boolean isVirtual() {
         return this.virtual;
     }
-    
+
     public abstract String[] getPermissions(String world);
-    
+
     public abstract Map<String, String[]> getAllPermissions();
-    
+
     public abstract Map<String, Map<String, String>> getAllOptions();
-    
+
     public abstract Map<String, String> getOptions(String world);
-    
+
     public abstract String getOption(String permission, String world, boolean inheritance);
-    
+
     public abstract void setOption(String permission, String value, String world);
-    
+
     public abstract void addPermission(String permission, String world);
-    
+
     public abstract void removePermission(String permission, String world);
-    
+
     public abstract void setPermissions(String[] permissions, String world);
-    
+
     public abstract void save();
-    
+
     public abstract void remove();
-    
+
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
@@ -154,28 +154,27 @@ public abstract class PermissionEntity {
         }
         return true;
     }
-    
+
     @Override
     public int hashCode() {
         int hash = 7;
         hash = 89 * hash + (this.name != null ? this.name.hashCode() : 0);
         return hash;
     }
-    
     /**
      * Pattern cache
      */
-    protected static HashMap<String, Pattern> patternCache = new HashMap<String, Pattern>();
+    protected static HashMap<String, Automaton> patternCache = new HashMap<String, Automaton>();
 
-    public static Pattern getPattern(String expression) {
+    public static boolean isMatches(String expression, String permission) {
         if (expression.startsWith("-")) {
             expression = expression.substring(1);
         }
 
         if (!patternCache.containsKey(expression)) {
-            patternCache.put(expression, Pattern.compile(expression.replace(".", "\\.").replace("*", "(.*)")));
+            patternCache.put(expression, new RegExp(expression.replace(".", "\\.").replace("*", "(.*)")).toAutomaton());
         }
 
-        return patternCache.get(expression);
+        return patternCache.get(expression).run(permission);
     }
 }
