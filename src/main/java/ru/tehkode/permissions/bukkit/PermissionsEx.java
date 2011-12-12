4ad5030ec0cb38b99ab8e98bc53265118f7bb441
t3hk0d3 <clouster@yandex.ru>
2011-12-12 21:56:26 +0400

    Fixed formatting and fixed user cache validation events (removed unnecessary ones)

--- src/main/java/ru/tehkode/permissions/bukkit/PermissionsEx.java
+++ src/main/java/ru/tehkode/permissions/bukkit/PermissionsEx.java
@@ -51,188 +51,165 @@ import ru.tehkode.permissions.commands.CommandsManager;
  */
 public class PermissionsEx extends JavaPlugin {
 
-    protected static final String configFile = "config.yml";
-    protected static final Logger logger = Logger.getLogger("Minecraft");
-    protected PermissionManager permissionsManager;
-    protected CommandsManager commandsManager;
-    protected Configuration config;
-    protected BukkitPermissions superms;
+	protected static final String configFile = "config.yml";
+	protected static final Logger logger = Logger.getLogger("Minecraft");
+	protected PermissionManager permissionsManager;
+	protected CommandsManager commandsManager;
+	protected Configuration config;
+	protected BukkitPermissions superms;
 
-    public PermissionsEx() {
-        super();
+	public PermissionsEx() {
+		super();
 
-        PermissionBackend.registerBackendAlias("sql", SQLBackend.class);
-        PermissionBackend.registerBackendAlias("file", FileBackend.class);
+		PermissionBackend.registerBackendAlias("sql", SQLBackend.class);
+		PermissionBackend.registerBackendAlias("file", FileBackend.class);
 
-        logger.log(Level.INFO, "[PermissionsEx] PermissionEx plugin initialized.");
-    }
+		logger.log(Level.INFO, "[PermissionsEx] PermissionEx plugin initialized.");
+	}
 
-    @Override
-    public void onLoad() {
-        this.config = this.loadConfig(configFile);
-        this.commandsManager = new CommandsManager(this);
-        this.permissionsManager = new PermissionManager(this.config);
-    }
+	@Override
+	public void onLoad() {
+		this.config = this.loadConfig(configFile);
+		this.commandsManager = new CommandsManager(this);
+		this.permissionsManager = new PermissionManager(this.config);
+	}
 
-    @Override
-    public void onEnable() {
-        // Register commands
-        this.commandsManager.register(new UtilityCommands());
-        this.commandsManager.register(new UserCommands());
-        this.commandsManager.register(new GroupCommands());
-        this.commandsManager.register(new PromotionCommands());
-        this.commandsManager.register(new WorldCommands());
+	@Override
+	public void onEnable() {
+		// Register commands
+		this.commandsManager.register(new UtilityCommands());
+		this.commandsManager.register(new UserCommands());
+		this.commandsManager.register(new GroupCommands());
+		this.commandsManager.register(new PromotionCommands());
+		this.commandsManager.register(new WorldCommands());
 
-        // Register Player permissions cleaner
-        this.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_QUIT, new PlayerEventsListener(), Priority.Normal, this);
-        // Multiworld cache cleaner
-        this.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_RESPAWN, new PlayerEventsListener(), Priority.Lowest, this);
-        this.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_TELEPORT, new PlayerEventsListener(), Priority.Lowest, this);
-        this.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_PORTAL, new PlayerEventsListener(), Priority.Lowest, this);
+		// Register Player permissions cleaner
+		this.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_QUIT, new PlayerEventsListener(), Priority.Normal, this);
 
-        //register service
-        this.getServer().getServicesManager().register(PermissionManager.class, this.permissionsManager, this, ServicePriority.Normal);
+		//register service
+		this.getServer().getServicesManager().register(PermissionManager.class, this.permissionsManager, this, ServicePriority.Normal);
 
-        // Bukkit permissions
-        ConfigurationNode dinnerpermsConfig = this.config.getNode("permissions.superperms");
+		// Bukkit permissions
+		ConfigurationNode dinnerpermsConfig = this.config.getNode("permissions.superperms");
 
-        if (dinnerpermsConfig == null) {
-            this.config.setProperty("permissions.superperms", new HashMap<String, Object>());
-            dinnerpermsConfig = this.config.getNode("permissions.superperms");
-        }
+		if (dinnerpermsConfig == null) {
+			this.config.setProperty("permissions.superperms", new HashMap<String, Object>());
+			dinnerpermsConfig = this.config.getNode("permissions.superperms");
+		}
 
-        this.superms = new BukkitPermissions(this, dinnerpermsConfig);
-        
-        this.superms.updateAllPlayers();
+		this.superms = new BukkitPermissions(this, dinnerpermsConfig);
 
-        this.config.save();
+		this.superms.updateAllPlayers();
 
-        logger.log(Level.INFO, "[PermissionsEx] v" + this.getDescription().getVersion() + " enabled");
-    }
+		this.config.save();
 
-    @Override
-    public void onDisable() {
-		if(this.permissionsManager != null){
+		logger.log(Level.INFO, "[PermissionsEx] v" + this.getDescription().getVersion() + " enabled");
+	}
+
+	@Override
+	public void onDisable() {
+		if (this.permissionsManager != null) {
 			this.permissionsManager.reset();
 		}
 
-        this.getServer().getServicesManager().unregister(PermissionManager.class, this.permissionsManager);
+		this.getServer().getServicesManager().unregister(PermissionManager.class, this.permissionsManager);
 
-        logger.log(Level.INFO, "[PermissionsEx] v" + this.getDescription().getVersion() + " disabled successfully.");
-    }
+		logger.log(Level.INFO, "[PermissionsEx] v" + this.getDescription().getVersion() + " disabled successfully.");
+	}
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
-        PluginDescriptionFile pdf = this.getDescription();
-        if (args.length > 0) {
-            return this.commandsManager.execute(sender, command, args);
-        } else {
-            if (sender instanceof Player) {
-                sender.sendMessage("[" + ChatColor.RED + "PermissionsEx" + ChatColor.WHITE + "] version [" + ChatColor.BLUE + pdf.getVersion() + ChatColor.WHITE + "]");
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+		PluginDescriptionFile pdf = this.getDescription();
+		if (args.length > 0) {
+			return this.commandsManager.execute(sender, command, args);
+		} else {
+			if (sender instanceof Player) {
+				sender.sendMessage("[" + ChatColor.RED + "PermissionsEx" + ChatColor.WHITE + "] version [" + ChatColor.BLUE + pdf.getVersion() + ChatColor.WHITE + "]");
 
-                return !this.permissionsManager.has((Player) sender, "permissions.manage");
-            } else {
-                sender.sendMessage("[PermissionsEx] version [" + pdf.getVersion() + "]");
+				return !this.permissionsManager.has((Player) sender, "permissions.manage");
+			} else {
+				sender.sendMessage("[PermissionsEx] version [" + pdf.getVersion() + "]");
 
-                return false;
-            }
-        }
-    }
-    
-    public static Plugin getPlugin(){
-        return Bukkit.getServer().getPluginManager().getPlugin("PermissionsEx");
-    }
-    
-    public static boolean isAvailable(){
-        Plugin plugin = getPlugin();
-        
-        return !(plugin == null || !(plugin instanceof PermissionsEx) || ((PermissionsEx) plugin).permissionsManager == null);
-    }
+				return false;
+			}
+		}
+	}
 
-    public static PermissionManager getPermissionManager() {
-        Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("PermissionsEx");
-        
-        if (!isAvailable()) {
-            throw new RuntimeException("Permissions manager is not accessable. Is the PermissionsEx plugin enabled?");
-        }
+	public static Plugin getPlugin() {
+		return Bukkit.getServer().getPluginManager().getPlugin("PermissionsEx");
+	}
 
-        return ((PermissionsEx) plugin).permissionsManager;
-    }
+	public static boolean isAvailable() {
+		Plugin plugin = getPlugin();
 
-    public static PermissionUser getUser(Player player) {
-        return getPermissionManager().getUser(player);
-    }
+		return !(plugin == null || !(plugin instanceof PermissionsEx) || ((PermissionsEx) plugin).permissionsManager == null);
+	}
 
-    public static PermissionUser getUser(String name) {
-        return getPermissionManager().getUser(name);
-    }
+	public static PermissionManager getPermissionManager() {
+		Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("PermissionsEx");
 
-    public boolean has(Player player, String permission) {
-        return this.permissionsManager.has(player, permission);
-    }
+		if (!isAvailable()) {
+			throw new RuntimeException("Permissions manager is not accessable. Is the PermissionsEx plugin enabled?");
+		}
 
-    public boolean has(Player player, String permission, String world) {
-        return this.permissionsManager.has(player, permission, world);
-    }
+		return ((PermissionsEx) plugin).permissionsManager;
+	}
 
-    @Override
-    public org.bukkit.util.config.Configuration getConfiguration() {
-        return this.config;
-    }
-    
-    protected final Configuration loadConfig(String name) {
-        File dataFolder = getDataFolder();
+	public static PermissionUser getUser(Player player) {
+		return getPermissionManager().getUser(player);
+	}
 
-        if (dataFolder == null) {
-            dataFolder = new File("plugins/PermissionsEx/");
-        }
+	public static PermissionUser getUser(String name) {
+		return getPermissionManager().getUser(name);
+	}
 
-        File configurationFile = new File(dataFolder, configFile);
-        Configuration configuration;
-        if (!configurationFile.exists()) {
-            try {
-                if (!dataFolder.exists()) {
-                    dataFolder.mkdirs();
-                }
-                configurationFile.createNewFile(); // Try to create new one
-                configuration = new Configuration(configurationFile);
-                configuration.setProperty("permissions.basedir", dataFolder.getPath());
-                configuration.save();
-            } catch (IOException e) {
-                // And if failed (ex.: not enough rights) - catch exception
-                throw new RuntimeException(e); // Rethrow exception
-            }
-        } else {
-            configuration = new Configuration(configurationFile);
-            configuration.load();
-        }
-        return configuration;
-    }
+	public boolean has(Player player, String permission) {
+		return this.permissionsManager.has(player, permission);
+	}
 
-    public class PlayerEventsListener extends PlayerListener {
+	public boolean has(Player player, String permission, String world) {
+		return this.permissionsManager.has(player, permission, world);
+	}
 
-        @Override
-        public void onPlayerQuit(PlayerQuitEvent event) {
-            getPermissionManager().resetUser(event.getPlayer().getName());
-        }
+	@Override
+	public org.bukkit.util.config.Configuration getConfiguration() {
+		return this.config;
+	}
 
-        protected void clearUserCache(PlayerEvent event) {
-            getPermissionManager().clearUserCache(event.getPlayer());
-        }
+	protected final Configuration loadConfig(String name) {
+		File dataFolder = getDataFolder();
 
-        @Override
-        public void onPlayerPortal(PlayerPortalEvent event) {
-            this.clearUserCache(event);
-        }
+		if (dataFolder == null) {
+			dataFolder = new File("plugins/PermissionsEx/");
+		}
 
-        @Override
-        public void onPlayerRespawn(PlayerRespawnEvent event) {
-            this.clearUserCache(event);
-        }
+		File configurationFile = new File(dataFolder, configFile);
+		Configuration configuration;
+		if (!configurationFile.exists()) {
+			try {
+				if (!dataFolder.exists()) {
+					dataFolder.mkdirs();
+				}
+				configurationFile.createNewFile(); // Try to create new one
+				configuration = new Configuration(configurationFile);
+				configuration.setProperty("permissions.basedir", dataFolder.getPath());
+				configuration.save();
+			} catch (IOException e) {
+				// And if failed (ex.: not enough rights) - catch exception
+				throw new RuntimeException(e); // Rethrow exception
+			}
+		} else {
+			configuration = new Configuration(configurationFile);
+			configuration.load();
+		}
+		return configuration;
+	}
 
-        @Override
-        public void onPlayerTeleport(PlayerTeleportEvent event) {
-            this.clearUserCache(event);
-        }
-    }
+	public class PlayerEventsListener extends PlayerListener {
+
+		@Override
+		public void onPlayerQuit(PlayerQuitEvent event) {
+			getPermissionManager().resetUser(event.getPlayer().getName());
+		}
+	}
 }
