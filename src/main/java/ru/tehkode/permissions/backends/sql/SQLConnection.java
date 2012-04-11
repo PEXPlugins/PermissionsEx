5146c11b0491911bf3ef05be2d14f87574970f34
t3hk0d3 <clouster@yandex.ru>
2012-04-11 23:34:32 +0400

    Fixed formatting

--- src/main/java/ru/tehkode/permissions/backends/sql/SQLConnection.java
+++ src/main/java/ru/tehkode/permissions/backends/sql/SQLConnection.java
@@ -39,180 +39,180 @@ import ru.tehkode.utils.StringUtils;
  */
 public class SQLConnection {
 
-    protected static Pattern placeholderPattern = Pattern.compile("\\`([^\\`]+)\\`");
-    protected Connection db;
-    protected String uri;
-    protected String user;
-    protected String password;
-    protected String dbDriver;
-    protected Map<String, String> aliases = new HashMap<String, String>();
+	protected static Pattern placeholderPattern = Pattern.compile("\\`([^\\`]+)\\`");
+	protected Connection db;
+	protected String uri;
+	protected String user;
+	protected String password;
+	protected String dbDriver;
+	protected Map<String, String> aliases = new HashMap<String, String>();
 
-    public SQLConnection(String uri, String user, String password, String dbDriver) {
-        try {
+	public SQLConnection(String uri, String user, String password, String dbDriver) {
+		try {
 
-            Class.forName(getDriverClass(dbDriver)).newInstance();
+			Class.forName(getDriverClass(dbDriver)).newInstance();
 
-            this.uri = uri;
-            this.user = user;
-            this.password = password;
+			this.uri = uri;
+			this.user = user;
+			this.password = password;
 
-            this.connect();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+			this.connect();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    public void setAlias(String tableName, String alias) {
-        this.aliases.put(tableName, alias);
-    }
+	public void setAlias(String tableName, String alias) {
+		this.aliases.put(tableName, alias);
+	}
 
-    public String getAlias(String tableName) {
-        if (this.aliases.containsKey(tableName)) {
-            return this.aliases.get(tableName);
-        }
+	public String getAlias(String tableName) {
+		if (this.aliases.containsKey(tableName)) {
+			return this.aliases.get(tableName);
+		}
 
-        return tableName;
-    }
+		return tableName;
+	}
 
-    public ResultSet selectQuery(String sql, Object... params) throws SQLException {
-        this.checkConnection();
+	public ResultSet selectQuery(String sql, Object... params) throws SQLException {
+		this.checkConnection();
 
-        PreparedStatement stmt = this.db.prepareStatement(this.prepareQuery(sql));
+		PreparedStatement stmt = this.db.prepareStatement(this.prepareQuery(sql));
 
-        if (params != null) {
-            this.bindParams(stmt, params);
-        }
+		if (params != null) {
+			this.bindParams(stmt, params);
+		}
 
-        return stmt.executeQuery();
-    }
+		return stmt.executeQuery();
+	}
 
-    public Object selectQueryOne(String sql, Object fallback, Object... params) {
-        try {
-            this.checkConnection();
+	public Object selectQueryOne(String sql, Object fallback, Object... params) {
+		try {
+			this.checkConnection();
 
-            ResultSet result = this.selectQuery(sql, params);
+			ResultSet result = this.selectQuery(sql, params);
 
-            if (!result.next()) {
-                return fallback;
-            }
+			if (!result.next()) {
+				return fallback;
+			}
 
-            return result.getObject(1);
+			return result.getObject(1);
 
-        } catch (SQLException e) {
-            Logger.getLogger("Minecraft").severe("SQL Error: " + e.getMessage());
-        }
+		} catch (SQLException e) {
+			Logger.getLogger("Minecraft").severe("SQL Error: " + e.getMessage());
+		}
 
-        return fallback;
-    }
+		return fallback;
+	}
 
-    public void updateQuery(String sql, Object... params) {
-        try {
-            this.checkConnection();
+	public void updateQuery(String sql, Object... params) {
+		try {
+			this.checkConnection();
 
-            PreparedStatement stmt = this.db.prepareStatement(this.prepareQuery(sql));
+			PreparedStatement stmt = this.db.prepareStatement(this.prepareQuery(sql));
 
-            if (params != null) {
-                this.bindParams(stmt, params);
-            }
+			if (params != null) {
+				this.bindParams(stmt, params);
+			}
 
-            stmt.executeUpdate();
+			stmt.executeUpdate();
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    public void insert(String table, String[] fields, List<Object[]> rows) throws SQLException {
-        this.checkConnection();
+	public void insert(String table, String[] fields, List<Object[]> rows) throws SQLException {
+		this.checkConnection();
 
-        String[] fieldPlaceholders = new String[fields.length];
-        Arrays.fill(fieldPlaceholders, "?");
-        String sql = "INSERT INTO `" + table + "` (`" + StringUtils.implode(fields, "`, `") + "`) VALUES (" + StringUtils.implode(fieldPlaceholders, ", ") + ");";
+		String[] fieldPlaceholders = new String[fields.length];
+		Arrays.fill(fieldPlaceholders, "?");
+		String sql = "INSERT INTO `" + table + "` (`" + StringUtils.implode(fields, "`, `") + "`) VALUES (" + StringUtils.implode(fieldPlaceholders, ", ") + ");";
 
-        PreparedStatement stmt = this.db.prepareStatement(this.prepareQuery(sql));
+		PreparedStatement stmt = this.db.prepareStatement(this.prepareQuery(sql));
 
-        for (Object[] params : rows) {
-            this.bindParams(stmt, params);
-            stmt.execute();
-        }
-    }
+		for (Object[] params : rows) {
+			this.bindParams(stmt, params);
+			stmt.execute();
+		}
+	}
 
-    public boolean isTableExist(String tableName) {
-        try {
-            this.checkConnection();
+	public boolean isTableExist(String tableName) {
+		try {
+			this.checkConnection();
 
-            return this.db.getMetaData().getTables(null, null, this.getAlias(tableName), null).next();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    
-    public boolean isFieldExists(String tableName, String fieldName){
-        try {
-            this.checkConnection();
-            
-            
-            return this.db.getMetaData().getColumns(null, null, this.getAlias(tableName), fieldName).next();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+			return this.db.getMetaData().getTables(null, null, this.getAlias(tableName), null).next();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    protected void checkConnection() throws SQLException {
-        if (this.db.getClass().getName().startsWith("org.sqlite")) {
-            return;
-        }
+	public boolean isFieldExists(String tableName, String fieldName) {
+		try {
+			this.checkConnection();
 
-        if (!this.db.isValid(0)) {
-            Logger.getLogger("Minecraft").warning("Lost connection with sql server. Reconnecting.");
-            this.connect();
-        }
-    }
 
-    protected final void connect() throws SQLException {
-        Logger.getLogger("Minecraft").info("[PermissionsEx-SQL] Connecting to database \"" + this.uri + "\"");
-        db = DriverManager.getConnection("jdbc:" + uri, user, password);
-    }
+			return this.db.getMetaData().getColumns(null, null, this.getAlias(tableName), fieldName).next();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    protected static String getDriverClass(String alias) {
+	protected void checkConnection() throws SQLException {
+		if (this.db.getClass().getName().startsWith("org.sqlite")) {
+			return;
+		}
 
-        if (alias.equals("mysql")) {
-            alias = "com.mysql.jdbc.Driver";
-        } else if (alias.equals("sqlite")) {
-            alias = "org.sqlite.JDBC";
-        } else if (alias.equals("postgre")) {
-            alias = "org.postgresql.Driver";
-        }
-        
-        return alias;
-    }
+		if (!this.db.isValid(0)) {
+			Logger.getLogger("Minecraft").warning("Lost connection with sql server. Reconnecting.");
+			this.connect();
+		}
+	}
 
-    protected void bindParams(PreparedStatement stmt, Object[] params) throws SQLException {
-        for (int i = 1; i <= params.length; i++) {
-            Object param = params[i - 1];
-            stmt.setObject(i, param);
-        }
-    }
+	protected final void connect() throws SQLException {
+		Logger.getLogger("Minecraft").info("[PermissionsEx-SQL] Connecting to database \"" + this.uri + "\"");
+		db = DriverManager.getConnection("jdbc:" + uri, user, password);
+	}
 
-    protected String prepareQuery(String sql) {
-        Matcher match = placeholderPattern.matcher(sql);
+	protected static String getDriverClass(String alias) {
 
-        while (match.find()) {
-            sql = sql.replace(match.group(0), "`" + this.getAlias(match.group(1)) + "`");
-        }
-      
-        return sql;
-    }
+		if (alias.equals("mysql")) {
+			alias = "com.mysql.jdbc.Driver";
+		} else if (alias.equals("sqlite")) {
+			alias = "org.sqlite.JDBC";
+		} else if (alias.equals("postgre")) {
+			alias = "org.postgresql.Driver";
+		}
 
-    @Override
-    protected void finalize() throws Throwable {
-        super.finalize();
-        try {
-            db.close();
-        } catch (SQLException e) {
-            Logger.getLogger("Minecraft").log(Level.WARNING, "Error while disconnecting from database: {0}", e.getMessage());
-        } finally {
-            db = null;
-        }
-    }
+		return alias;
+	}
+
+	protected void bindParams(PreparedStatement stmt, Object[] params) throws SQLException {
+		for (int i = 1; i <= params.length; i++) {
+			Object param = params[i - 1];
+			stmt.setObject(i, param);
+		}
+	}
+
+	protected String prepareQuery(String sql) {
+		Matcher match = placeholderPattern.matcher(sql);
+
+		while (match.find()) {
+			sql = sql.replace(match.group(0), "`" + this.getAlias(match.group(1)) + "`");
+		}
+
+		return sql;
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		super.finalize();
+		try {
+			db.close();
+		} catch (SQLException e) {
+			Logger.getLogger("Minecraft").log(Level.WARNING, "Error while disconnecting from database: {0}", e.getMessage());
+		} finally {
+			db = null;
+		}
+	}
 }
