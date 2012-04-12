14f69a1114f471d58d39cac909ed35b544933519
Robert <phoenix7782@gmail.com>
2012-04-11 20:25:26 -0400

    Fixed implode, add documentation and simplify code

--- src/main/java/ru/tehkode/utils/StringUtils.java
+++ src/main/java/ru/tehkode/utils/StringUtils.java
@@ -21,11 +21,17 @@ package ru.tehkode.utils;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.io.StringWriter;
 import java.util.List;
 
 public class StringUtils {
-
+    
+    /**
+     * Returns the given array joined by a separator.
+     *
+     * @param array an array of strings to join
+     * @param separator a string to insert between the array elements
+     * @return the full string result
+     */
     public static String implode(String[] array, String separator) {
         if (array.length == 0) {
             return "";
@@ -40,7 +46,14 @@ public class StringUtils {
 
         return buffer.substring(separator.length()).trim();
     }
-
+    
+    /**
+     * Returns the elements joined by a separator.
+     *
+     * @param list a List object to join together
+     * @param separator a string to insert between the list elements
+     * @return the full string result
+     */
     public static String implode(List<?> list, String separator) {
         if (list.isEmpty()) {
             return "";
@@ -62,10 +75,16 @@ public class StringUtils {
 
         return builder.toString();
     }
-
+    
+    /**
+     * Return a stream's complete input as a string, or null if the InputStream is null
+     *
+     * @param is an InputStream to read from
+     * @return a String representing the input read from the InputStream
+     */
     public static String readStream(InputStream is) throws Exception {
         if (is != null) {
-            StringWriter writer = new StringWriter();
+            StringBuilder builder = new StringBuilder();
 
             try {
                 Reader reader = new InputStreamReader(is, "UTF-8");
@@ -73,36 +92,46 @@ public class StringUtils {
                 char[] buffer = new char[128];
                 int read = 0;
                 while ((read = reader.read(buffer)) > 0) {
-                    writer.write(buffer, 0, read);
+                    builder.append(buffer, 0, read);
                 }
             } finally {
                 is.close();
             }
 
-            return writer.toString();
+            return builder.toString();
         }
 
         return null;
     }
-
+    
+    /**
+     * Repeat a string a given number of times.
+     * 
+     * @param str the string to repeat
+     * @param times the number of times to repeat the string
+     * @return the completed repeating
+     */
     public static String repeat(String str, int times) {
-        final StringBuilder buffer = new StringBuilder(times * str.length());
+        StringBuilder buffer = new StringBuilder(times * str.length());
         for (int i = 0; i < times; i++) {
             buffer.append(str);
         }
         return buffer.toString();
 
     }
-
+    
+    /**
+     * Parse a string to an integer value, using a given default on fail
+     *
+     * @param value a String to parse to an int
+     * @param defaultValue the default, used when 'value' is not an integer
+     * @return the parsed value
+     */
     public static int toInteger(String value, int defaultValue) {
-        if (value == null || value.isEmpty()) {
-            return defaultValue;
-        }
-
+        int ret = defaultValue;
         try {
-            return Integer.parseInt(value);
-        } catch (NumberFormatException e) {
-            return defaultValue;
-        }
+            ret = Integer.parseInt(value);
+        } catch (NumberFormatException e) {}
+        return ret;
     }
 }
