cbfd01590e83b22fdd4d3bafb26248ca51b3dbd0
zml <zach@zachsthings.com>
2014-04-16 18:35:00 -0700

    Fix autoreconnect for MySQL databases

--- src/main/java/ru/tehkode/permissions/backends/sql/SQLBackend.java
+++ src/main/java/ru/tehkode/permissions/backends/sql/SQLBackend.java
@@ -81,6 +81,9 @@ public class SQLBackend extends PermissionBackend {
 		this.ds.setPassword(dbPassword);
 		this.ds.setMaxActive(20);
 		this.ds.setMaxWait(200); // 4 ticks
+		if (this.dbDriver.equals("mysql")) {
+			this.ds.addConnectionProperty("autoReconnect", "true");
+		}
 
 		try (SQLConnection conn = getSQL()) {
 			conn.checkConnection();
