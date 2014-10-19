71cfcc2cfb2603555e5430c43363c3eabd321138
zml <zach@zachsthings.com>
2014-10-19 16:51:03 -0700

    Fix compatibility of accountsclient script with plain sh

--- etc/build-accountsclient.sh
+++ etc/build-accountsclient.sh
@@ -3,7 +3,7 @@ set -e
 
 DESTDIR="AccountsClient"
 git clone https://github.com/Mojang/AccountsClient.git $DESTDIR
-pushd $DESTDIR
+cd $DESTDIR
 
 sed -i "s/testLogging\.showStandardStreams = true//" build.gradle
 sed -i "s/apply plugin: 'idea'/apply plugin: 'idea'\napply plugin: 'maven'\ngroup = 'com.mojang'/" build.gradle
@@ -11,5 +11,5 @@ echo "rootProject.name = 'AccountsClient'" >> settings.gradle
 
 gradle clean install
 
+cd ..
 rm -rf $DESTDIR
-popd
