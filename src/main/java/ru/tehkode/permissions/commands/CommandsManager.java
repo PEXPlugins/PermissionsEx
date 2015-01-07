a196739d72a6106cd1fa028d153cda76f0e1845f
zml <zml@aoeu.xyz>
2015-01-06 17:28:06 -0800

    Correct CommandsManager exception handling

--- src/main/java/ru/tehkode/permissions/commands/CommandsManager.java
+++ src/main/java/ru/tehkode/permissions/commands/CommandsManager.java
@@ -114,7 +114,7 @@ public class CommandsManager {
 				sender.sendMessage("Autocomplete for <" + autocomplete.getArgName() + ">:");
 				sender.sendMessage("    " + StringUtils.implode(autocomplete.getChoices(), "   "));
 			} else {
-				throw new RuntimeException(e.getTargetException());
+				throw new RuntimeException(e.getCause());
 			}
 		} catch (Exception e) {
 			plugin.getLogger().severe("There is bogus command handler for " + command.getName() + " command. (Is appropriate plugin is update?)");
