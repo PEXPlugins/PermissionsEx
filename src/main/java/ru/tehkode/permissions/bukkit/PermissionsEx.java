5cb62e8974659488b2163e70e40296ebf740f319
Samistine <samuel@samistine.com>
2016-02-06 14:57:35 -0500

    Match onPlayerQuit formatting

--- src/main/java/ru/tehkode/permissions/bukkit/PermissionsEx.java
+++ src/main/java/ru/tehkode/permissions/bukkit/PermissionsEx.java
@@ -444,11 +444,10 @@ public class PermissionsEx extends JavaPlugin implements NativeInterface {
 					if (!event.getPlayer().getName().equals(user.getOption("name"))) { // Update name only if user exists in config
 						user.setOption("name", event.getPlayer().getName());
 					}
-					if (!config.shouldLogPlayers() || user.isVirtual()) {
-						return;
+					if (config.shouldLogPlayers() || !user.isVirtual()) {
+						user.setOption("last-login-time", Long.toString(System.currentTimeMillis() / 1000L));
+						// user.setOption("last-login-ip", event.getPlayer().getAddress().getAddress().getHostAddress()); // somehow this won't work
 					}
-					user.setOption("last-login-time", Long.toString(System.currentTimeMillis() / 1000L));
-					// user.setOption("last-login-ip", event.getPlayer().getAddress().getAddress().getHostAddress()); // somehow this won't work
 				}
 			} catch (Throwable t) {
 				ErrorReport.handleError("While login cleanup event", t);
