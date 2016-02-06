e67d399d9dbb41118f457de12dedf108624f2ff4
Samistine <samuel@samistine.com>
2016-02-06 14:56:11 -0500

    Don't create a player if he's virtual

--- src/main/java/ru/tehkode/permissions/bukkit/PermissionsEx.java
+++ src/main/java/ru/tehkode/permissions/bukkit/PermissionsEx.java
@@ -444,7 +444,7 @@ public class PermissionsEx extends JavaPlugin implements NativeInterface {
 					if (!event.getPlayer().getName().equals(user.getOption("name"))) { // Update name only if user exists in config
 						user.setOption("name", event.getPlayer().getName());
 					}
-					if (!config.shouldLogPlayers()) {
+					if (!config.shouldLogPlayers() || user.isVirtual()) {
 						return;
 					}
 					user.setOption("last-login-time", Long.toString(System.currentTimeMillis() / 1000L));
