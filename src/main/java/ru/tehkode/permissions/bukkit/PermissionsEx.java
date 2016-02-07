7194c78950bcefe7930adc2eda47e0c5682e0263
Samistine <Samistine@users.noreply.github.com>
2016-02-06 22:59:52 -0500

    Use && instead of ||

--- src/main/java/ru/tehkode/permissions/bukkit/PermissionsEx.java
+++ src/main/java/ru/tehkode/permissions/bukkit/PermissionsEx.java
@@ -444,7 +444,7 @@ public class PermissionsEx extends JavaPlugin implements NativeInterface {
 					if (!event.getPlayer().getName().equals(user.getOption("name"))) { // Update name only if user exists in config
 						user.setOption("name", event.getPlayer().getName());
 					}
-					if (config.shouldLogPlayers() || !user.isVirtual()) {
+					if (config.shouldLogPlayers() && !user.isVirtual()) {
 						user.setOption("last-login-time", Long.toString(System.currentTimeMillis() / 1000L));
 						// user.setOption("last-login-ip", event.getPlayer().getAddress().getAddress().getHostAddress()); // somehow this won't work
 					}
