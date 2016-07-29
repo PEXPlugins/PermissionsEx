21a336dea99c9d68d94a1a2349e56e6031f8719b
zml <zml@aoeu.xyz>
2016-07-29 12:06:20 -0700

    Allow adding version suffix to plugin meta

--- permissionsex-sponge/src/main/java-templates/ninja/leaping/permissionsex/sponge/PomData.java
+++ permissionsex-sponge/src/main/java-templates/ninja/leaping/permissionsex/sponge/PomData.java
@@ -3,5 +3,5 @@ package ninja.leaping.permissionsex.sponge;
 class PomData {
 	public static final String ARTIFACT_ID = "${project.parent.groupId}";
 	public static final String NAME = "${project.parent.name}";
-	public static final String VERSION = "${project.version}";
+	public static final String VERSION = "${project.version}${version.suffix}";
 }
