db177099989629339848fecfe01058b1febc5c86
Zach Levis <zml@aoeu.xyz>
2020-03-29 13:51:17 -0700

    Work on UltraPerms conversion

---
+++ permissionsex-core/src/main/kotlin/ca/stellardrift/permissionsex/backend/conversion/ultraperms/UltraPermissionsDataStore.kt
@@ -0,0 +1,49 @@
+/*
+ * PermissionsEx
+ * Copyright (C) zml and PermissionsEx contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ca.stellardrift.permissionsex.backend.conversion.ultraperms
+
+import ca.stellardrift.permissionsex.BaseDirectoryScope
+import ca.stellardrift.permissionsex.PermissionsEx
+import ca.stellardrift.permissionsex.backend.conversion.ConversionProvider
+import ca.stellardrift.permissionsex.backend.conversion.ConversionResult
+import java.awt.Component
+import java.net.URL
+import java.nio.file.Files
+import java.util.UUID
+
+data class PermissionEntry(val holder: String, val permission: String, val positive: Boolean, val expiry: Long)
+data class ServerEntry(val name: String, val lastHeartbeat: Long)
+data class UserEntry(val name: String, val superadmin: Boolean, val groups: Map<String, Int>, val prefix: String, val suffix: String, val skull: URL)
+data class GroupEntry(val name: String, val serverId: UUID, val priority: Int, val default: Boolean, val icon: String /* Material */)
+
+class UltraPermissionsDataStore {
+    companion object : ConversionProvider {
+        override val name: Component
+            get() = TODO()//t("Ultra Permissions")
+
+        override fun listConversionOptions(pex: PermissionsEx<*>): List<ConversionResult> {
+            val ultraPermsDir =pex.getBaseDirectory(BaseDirectoryScope.JAR).resolve("UltraPermissions")
+            if (Files.exists(ultraPermsDir)) {
+                // TODO: prepare an instance of an UltraPerms configuration
+            }
+            return listOf()
+        }
+
+    }
+}
+
---
+++ permissionsex-core/src/main/kotlin/ca/stellardrift/permissionsex/backend/conversion/ultraperms/UltraPermissionsFile.kt
@@ -0,0 +1,118 @@
+/*
+ * PermissionsEx
+ * Copyright (C) zml and PermissionsEx contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ca.stellardrift.permissionsex.backend.conversion.ultraperms
+
+import ca.stellardrift.permissionsex.util.configurate.get
+import ninja.leaping.configurate.ConfigurationNode
+import ninja.leaping.configurate.SimpleConfigurationNode
+import ninja.leaping.configurate.gson.GsonConfigurationLoader
+import ninja.leaping.configurate.loader.ConfigurationLoader
+import ninja.leaping.configurate.yaml.YAMLConfigurationLoader
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.nio.charset.StandardCharsets
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.Base64
+import java.util.concurrent.Callable
+
+class UltraPermissionsFile(val path: Path) {
+    val loader: ConfigurationLoader<ConfigurationNode> = YAMLConfigurationLoader.builder().setPath(path).build()
+
+    var node: ConfigurationNode = loader.load()
+
+    fun reload() {
+        node = loader.load()
+    }
+
+    fun save() {
+        loader.save(node)
+    }
+
+    private fun ConfigurationNode.getUnwrappedBase64Value(): ConfigurationNode {
+        if (hasListChildren() || hasMapChildren()) {
+            return this
+        }
+
+        val value = string?.run {
+            Base64.getDecoder().decode(this)
+        } ?: return this
+
+
+        val jsonLoader = GsonConfigurationLoader.builder().apply {
+            source = Callable { BufferedReader(InputStreamReader(ByteArrayInputStream(value), StandardCharsets.UTF_8)) }
+        }.build()
+        return try {
+            jsonLoader.load()
+        } catch (e: IOException) {
+            println("Failed to deserialize entry ${key}: ${e.message}")
+            e.printStackTrace()
+            SimpleConfigurationNode.root().setValue(String(value, StandardCharsets.UTF_8))
+        }
+    }
+
+    private fun ConfigurationNode.setWrappedBase64Value(value: ConfigurationNode) {
+        val out = ByteArrayOutputStream(128)
+        val jsonLoader = GsonConfigurationLoader.builder().apply {
+            sink = Callable { BufferedWriter(OutputStreamWriter(out, StandardCharsets.UTF_8)) }
+        }.build()
+        jsonLoader.save(value)
+
+        this.value = Base64.getEncoder().encodeToString(out.toByteArray())
+    }
+
+    operator fun get(key: String): ConfigurationNode {
+        return node[key].getUnwrappedBase64Value()
+    }
+
+    operator fun set(key: String, value: ConfigurationNode) {
+        node[key].setWrappedBase64Value(value)
+    }
+
+    fun unwrapFile() {
+        if (!node.hasMapChildren()) {
+            return
+        }
+
+        node.childrenMap.forEach { (_, v) ->
+            v.value = v.getUnwrappedBase64Value()
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+   val filename = if (args.isNotEmpty()) {
+       args[0]
+   } else {
+       println(Paths.get(".").toAbsolutePath())
+       print("File: ")
+       readLine()!!
+   }
+
+    val path = Paths.get(filename)
+    val upReader =
+        UltraPermissionsFile(path)
+    upReader.unwrapFile()
+    upReader.save()
+    println("File $filename unwrapped!")
+}
